04325d5e85081c2fb43c4dd401bb5e0de4b847a0
clydecollab <roverclyde@gmail.com>
2013-08-09 06:36:11 -0300

    Comment added

--- src/ForLoopDemo.java
+++ src/ForLoopDemo.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ForLoopDemo {
+	//This is acomment made by clydecollab
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
